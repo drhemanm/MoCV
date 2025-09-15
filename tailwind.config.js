1c947a4cf04f07a2f09379aa28488e4feaf64009
drhemanm <heman.m@iafrica.solutions>
2025-09-15 14:05:41 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 2s infinite',
+      }
+    },
   },
   plugins: [],
-};
+}
